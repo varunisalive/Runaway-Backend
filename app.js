071a5d677d00d5029a9adde17c38caf74af83d08
varun acharya <>
2020-12-04 19:18:39 +0530

    Data added to Collection -> products

--- app.js
+++ app.js
@@ -3,25 +3,69 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
 const path = require("path");
+const MongoClient = require("mongodb").MongoClient;
 
 const app = express();
 
-mongoose.connect("mongodb://localhost:27017/runawayDB", {useNewUrlParser: true, useUnifiedTopology: true});
+var url = "mongodb://localhost:27017/runawayDB";
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connection.on('connection', () => {
+    console.log("Mongoose is connected");
+});
 
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 app.use(morgan('tiny'));
 
-app.use("/", function(req, res){
-   const data = {
-       date: 30/11/2020
-   } 
-   res.json(data);
+const productsSchema = new mongoose.Schema({
+    imgURL: String,
+    name: String,
+    rating: Number,
+    price: Number
+});
+
+const Product = mongoose.model("Product", productsSchema);
+
+// MongoClient.connect(url, function (err, db) {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         var shoes = [
+//             
+//         ];
+
+//         db.collection("products").insertMany(shoes, function (err, data) {
+//             if (err) {
+//                 console.log(err);
+//             } else {
+//                 console.log(data.ops);
+//             } db.close();
+//         });
+//         
+//     }
+    
+// })
+
+
+
+app.get("/products", function (req, res) {
+
+    Product.find({}, function(err, foundProducts){
+        if(err){
+            console.log(err);
+        } else {
+            if(foundProducts){
+                res.json(foundProducts);
+            } else {
+                console.log("Products not found");
+            }
+        }
+    });
 });
 
 port = process.env.PORT || 8080;
 
-app.listen(port, function(req, res){
+app.listen(port, function (req, res) {
     console.log(`The Server is running on PORT ${port}`);
 });
